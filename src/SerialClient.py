b290ff2985e4018c259a865618c1d44eaf598419
Jorge Vulgarin <adrivulgarin@hotmail.com>
2024-01-03 13:45:43 -0500

    fix: only one serial message at a time

--- src/SerialClient.py
+++ src/SerialClient.py
@@ -1,15 +1,23 @@
 import serial
 import threading
+from time import sleep
 
 
 class SerialClient:
     def __init__(self, port, bdrate):
         self.port = port
         self.bdrate = bdrate
+        self.moving = False
         self.channel = serial.Serial(self.port, self.bdrate)
         self.height = 1000
 
     def move(self, rel_height: int):
+        # TODO: add proper signals or lock
+        while self.moving:
+            print("Waiting for serial port to become avialable")
+            sleep(0.5)
+        self.moving = True
+
         def run_movement():
             print("moving motors")
             self.channel.write(
@@ -17,4 +25,5 @@ class SerialClient:
             final_height = self.channel.readline()
             final_height = int(final_height)
             self.height = final_height
+            self.moving = False
         threading.Thread(target=run_movement).start()
